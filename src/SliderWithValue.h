08d804e3f9e90c261a90e4640a81cb8c7a90932c
ds <ds@a27563e6-5939-db11-9576-004854876331>
2007-07-15 12:57:19 +0000

    Include prototype custom NSSlider descendant for showing percentage value in 'instant' tooltip.
    git-svn-id: svn+voltron://voltron/data/svn/MarcoPolo/trunk@791 a27563e6-5939-db11-9576-004854876331

---
+++ src/SliderWithValue.h
@@ -0,0 +1,32 @@
+//
+//  SliderWithValue.h
+//  MarcoPolo
+//
+//  Created by David Symonds on 15/07/07.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ToolTipTextField : NSTextField {
+}
+
+@end
+
+#pragma mark -
+
+@interface ToolTip : NSObject {
+	NSWindow *window;
+	NSTextField *textField;
+	NSDictionary *textAttributes;
+}
+
+@end
+
+#pragma mark -
+
+@interface SliderWithValue : NSSlider {
+	BOOL dragging;
+}
+
+@end
---
+++ src/SliderWithValue.m
@@ -0,0 +1,162 @@
+//
+//  SliderWithValue.m
+//  MarcoPolo
+//
+//  Created by David Symonds on 15/07/07.
+//  Large chunks from http://www.cocoadev.com/index.pl?ToolTip
+//
+
+#import "SliderWithValue.h"
+
+
+@implementation ToolTipTextField
+
+- (void)drawRect:(NSRect)aRect
+{
+	[super drawRect:aRect];
+
+	//[[NSColor colorWithCalibratedWhite:0.925 alpha:1.0] set];
+	//NSFrameRect(aRect);
+}
+
+@end
+
+#pragma mark -
+
+static ToolTip *sharedToolTip = nil;
+
+@interface ToolTip (Private)
+
+- (void)setString:(NSString *)string forEvent:(NSEvent *)theEvent;
+
+@end
+
+@implementation ToolTip
+
++ (void)setString:(NSString *)string forEvent:(NSEvent *)theEvent
+{
+	if (!sharedToolTip)
+		sharedToolTip = [[ToolTip alloc] init];
+
+	[sharedToolTip setString:string forEvent:theEvent];
+}
+
++ (void)release
+{
+	[sharedToolTip release];
+	sharedToolTip = nil;
+}
+
+- (id)init
+{
+	if (!(self = [super init]))
+		return nil;
+
+	// These size are not really import, just the relation between the two...
+	NSRect contentRect = { { 100, 100 }, { 100, 20 } };
+	NSRect textFieldFrame = { { 0, 0 }, { 100, 20 } };
+
+	window = [[NSWindow alloc] initWithContentRect:contentRect
+					     styleMask:NSBorderlessWindowMask
+					       backing:NSBackingStoreBuffered
+						 defer:YES];
+
+	[window setOpaque:NO];
+	[window setAlphaValue:0.80];
+	[window setBackgroundColor:[NSColor colorWithDeviceRed:1.0 green:0.96 blue:0.76 alpha:1.0]];
+	[window setHasShadow:YES];
+	[window setLevel:NSStatusWindowLevel];
+	[window setReleasedWhenClosed:YES];
+	[window orderFront:nil];
+
+	textField = [[ToolTipTextField alloc] initWithFrame:textFieldFrame];
+	[textField setEditable:NO];
+	[textField setSelectable:NO];
+	[textField setBezeled:NO];
+	[textField setBordered:NO];
+	[textField setDrawsBackground:NO];
+	[textField setAlignment:NSCenterTextAlignment];
+	[textField setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
+	[textField setFont:[NSFont toolTipsFontOfSize:[NSFont systemFontSize]]];
+	[[window contentView] addSubview:textField];
+
+	[textField setStringValue:@" "]; // Just having at least 1 char to allow the next message...
+	textAttributes = [[[textField attributedStringValue] attributesAtIndex:0 effectiveRange:nil] retain];
+
+	return self;
+}
+
+- (void)dealloc
+{
+	[window release];
+	[textAttributes release];
+
+	[super dealloc];
+}
+
+- (void)setString:(NSString *)string forEvent:(NSEvent *)theEvent
+{
+	NSSize size = [string sizeWithAttributes:textAttributes];
+	NSPoint cursorScreenPosition = [[theEvent window] convertBaseToScreen:[theEvent locationInWindow]];
+
+	[textField setStringValue:string];
+	[window setFrameTopLeftPoint:NSMakePoint(cursorScreenPosition.x + 10, cursorScreenPosition.y + 28)];
+
+	[window setContentSize:NSMakeSize(size.width + 20, size.height + 1)];
+}
+
+@end
+
+#pragma mark -
+
+@interface SliderWithValue (Private)
+
+- (NSString *)toolTipText;
+
+@end
+
+@implementation SliderWithValue
+
+- (NSString *)toolTipText
+{
+	// TODO: localised version of this (correct decimal separator, etc.)
+	return [NSString stringWithFormat:@"%.1f%%", [self doubleValue] * 100];
+}
+
+- (id)initWithCoder:(NSCoder *)decoder
+{
+	if (!(self = [super initWithCoder:decoder]))
+		return nil;
+
+	dragging = NO;
+
+	[self setContinuous:YES];
+	[self setAction:@selector(doUpdate:)];
+	[self setTarget:self];
+
+	return self;
+}
+
+- (void)doUpdate:(id)sender
+{
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+
+	[ToolTip setString:[self toolTipText] forEvent:[NSApp currentEvent]];
+
+	if ([[NSApp currentEvent] type] == NSLeftMouseUp)
+		[ToolTip release];
+}
+
+- (void)mouseDown:(NSEvent *)theEvent
+{
+	NSLog(@"%s", __PRETTY_FUNCTION__);
+
+	//NSPoint where = [self convertPoint:[theEvent locationInWindow] fromView:nil];
+	dragging = YES;
+
+	[ToolTip setString:[self toolTipText] forEvent:theEvent];
+
+	[super mouseDown:theEvent];
+}
+
+@end
