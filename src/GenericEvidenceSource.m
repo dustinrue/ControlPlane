bd9d5e672a3df98d6dd2749420c199b8330887c6
ds <ds@a27563e6-5939-db11-9576-004854876331>
2007-07-19 09:12:58 +0000

    Fix GenericEvidenceSource's parameter reading (from panel), and select the current (by parameter) selection if we're editing a rule.
    git-svn-id: svn+voltron://voltron/data/svn/MarcoPolo/trunk@832 a27563e6-5939-db11-9576-004854876331

--- src/GenericEvidenceSource.m
+++ src/GenericEvidenceSource.m
@@ -22,7 +22,7 @@
 {
 	NSMutableDictionary *dict = [super readFromPanel];
 
-	id sel = [ruleParameterController selection];
+	id sel = [[ruleParameterController arrangedObjects] objectAtIndex:[ruleParameterController selectionIndex]];
 	[dict setValue:[sel valueForKey:@"parameter"] forKey:@"parameter"];
 	[dict setValue:[sel valueForKey:@"type"] forKey:@"type"];
 	[dict setValue:[sel valueForKey:@"description"] forKey:@"description"];
@@ -46,7 +46,28 @@
 
 	[ruleParameterController removeObjects:[ruleParameterController arrangedObjects]];
 	[ruleParameterController addObjects:[self getSuggestions]];
-	[ruleParameterController selectNext:self];
+	if (![dict objectForKey:@"parameter"])
+		[ruleParameterController selectNext:self];
+	else {
+		// Pick the current parameter
+		NSEnumerator *en = [[ruleParameterController arrangedObjects] objectEnumerator];
+		unsigned int index = 0;
+		NSDictionary *elt;
+		NSObject *thisParam = [dict valueForKey:@"parameter"];
+		while ((elt = [en nextObject])) {
+			// TODO: Check that this test is correct!
+			if ([[elt valueForKey:@"parameter"] isEqualTo:thisParam])
+				break;
+			++index;
+		}
+		if (elt) {
+			// Found!
+			[ruleParameterController setSelectionIndex:index];
+		} else {
+			// Current rule isn't in the list: d'oh!
+			// TODO: should also push existing one in, if it isn't there
+		}
+	}
 }
 
 @end
