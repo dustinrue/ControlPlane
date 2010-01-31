04ec86931facadac1f84e90fdf9b4ca22f4a1624
Andreas Linde <mail@andreaslinde.de>
2010-01-31 13:49:55 +0100

    Add some more server error codes to the Mac framework header file

--- Mac/Classes/CrashReportSender.h
+++ Mac/Classes/CrashReportSender.h
@@ -35,7 +35,21 @@ typedef enum CrashAlertType {
 } CrashAlertType;
 
 typedef enum CrashReportStatus {
-	CrashReportStatusFailure = -1,
+	CrashReportStatusFailureVersionDiscontinued = -30,          // This app version is set to discontinued, no new crash reports accepted by the server
+	CrashReportStatusFailureXMLSenderVersionNotAllowed = -21,   // XML: Sender ersion string contains not allowed characters, only alphanumberical including space and . are allowed
+	CrashReportStatusFailureXMLVersionNotAllowed = -20,         // XML: Version string contains not allowed characters, only alphanumberical including space and . are allowed
+	CrashReportStatusFailureSQLAddSymbolicateTodo = -18,        // SQL for adding a symoblicate todo entry in the database failed
+	CrashReportStatusFailureSQLAddCrashlog = -17,               // SQL for adding crash log in the database failed
+	CrashReportStatusFailureSQLAddVersion = -16,                // SQL for adding a new version in the database failed
+	CrashReportStatusFailureSQLCheckVersionExists = -15,        // SQL for checking if the version is already added in the database failed
+	CrashReportStatusFailureSQLAddPattern = -14,                // SQL for creating a new pattern for this bug and set amount of occurrances to 1 in the database failed
+	CrashReportStatusFailureSQLCheckBugfixStatus = -13,         // SQL for checking the status of the bugfix version in the database failed
+	CrashReportStatusFailureSQLUpdatePatternOccurances = -12,   // SQL for updating the occurances of this pattern in the database failed
+	CrashReportStatusFailureSQLFindKnownPatterns = -11,         // SQL for getting all the known bug patterns for the current app version in the database failed
+	CrashReportStatusFailureSQLSearchAppName = -10,             // SQL for finding the bundle identifier in the database failed
+	CrashReportStatusFailureInvalidPostData = -3,               // the post request didn't contain valid data
+	CrashReportStatusFailureInvalidIncomingData = -2,           // incoming data may not be added, because e.g. bundle identifier wasn't found
+	CrashReportStatusFailureDatabaseNotAvailable = -1,          // database cannot be accessed, check hostname, username, password and database name settings in config.php
 	CrashReportStatusUnknown = 0,
 	CrashReportStatusAssigned = 1,
 	CrashReportStatusSubmitted = 2,
