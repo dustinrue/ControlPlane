550b029dd33a605cee36f06f82020e483983832c
James Moore <james@panic.com>
2011-05-16 16:20:53 -0700

    factor out the plotting

--- server/admin/groups.php
+++ server/admin/groups.php
@@ -244,86 +244,9 @@ mysql_close($link);
 
 ?>
 </div>
-
 <script type="text/javascript">
 $(document).ready(function(){
-    $.jqplot.config.enablePlugins = true;
-
-<?php
-    if (sizeof($crashvaluesarray) > 0) {
-        foreach ($crashvaluesarray as $key => $value) {
-            if ($crashvalues != "") $crashvalues = $crashvalues.", ";
-            $crashvalues .= "['".$key."', ".$value."]";
-        }
-
-?>
-    line1 = [<?php echo $crashvalues; ?>];
-    plot1 = $.jqplot('crashdiv', [line1], {
-        seriesDefaults: {showMarker:false},
-        series:[
-            {pointLabels:{
-                show: false
-            }}],
-        axes:{
-            xaxis:{
-                renderer:$.jqplot.DateAxisRenderer,
-                rendererOptions:{tickRenderer:$.jqplot.CanvasAxisTickRenderer},
-                tickOptions:{formatString:'%#d-%b'}
-            },
-            yaxis:{
-                min: 0,
-                tickOptions:{formatString:'%.0f'}
-            }
-        },
-        highlighter: {sizeAdjust: 7.5}
-    });
-<?php
-    }
-    
-    if ($platformticks != "") {
-?>
-    line1 = [<?php echo $platformvalues; ?>];
-    plot1 = $.jqplot('platformdiv', [line1], {
-        seriesDefaults: {
-                renderer:$.jqplot.BarRenderer
-            },
-        axes:{
-            xaxis:{
-                renderer:$.jqplot.CategoryAxisRenderer,
-                ticks:[<?php echo $platformticks; ?>]
-            },
-            yaxis:{
-                min: 0,
-                tickOptions:{formatString:'%.0f'}
-            }
-        },
-        highlighter: {show: false}
-    });
-<?php
-    }
-    
-    if ($osticks != "") { 
-?>
-   line1 = [<?php echo $osvalues; ?>];
-    plot1 = $.jqplot('osdiv', [line1], {
-        seriesDefaults: {
-                renderer:$.jqplot.BarRenderer
-            },
-        axes:{
-            xaxis:{
-                renderer:$.jqplot.CategoryAxisRenderer,
-                ticks:[<?php echo $osticks; ?>]
-            },
-            yaxis:{
-                min: 0,
-                tickOptions:{formatString:'%.0f'}
-            }
-        },
-        highlighter: {show: false}
-    });
-<?php
-    }
-?>
+<?php include "jqplot.php" ?>
 });
 </script>
 
