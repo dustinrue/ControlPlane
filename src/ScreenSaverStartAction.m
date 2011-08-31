669cd250aa50ba0323f4783be20b57f0d27745e1
David Jennes <davidjennes@10-0-21-151.kotnet.org>
2011-08-31 02:47:39 +0200

    labels/goto-- ScreenSaverStartAction now uses Cocoa API's (unfortunately it doesn't support ScriptingBridge)

--- src/ScreenSaverStartAction.m
+++ src/ScreenSaverStartAction.m
@@ -18,20 +18,29 @@
 		return NSLocalizedString(@"Stopping screen saver.", @"");
 }
 
-- (BOOL)execute:(NSString **)errorString
-{
-	NSString *script = [NSString stringWithFormat:@"tell application \"ScreenSaverEngine\" to %@",
-		(turnOn ? @"activate" : @"quit")];
-
-	if (![self executeAppleScript:script]) {
-		if (turnOn)
-			*errorString = NSLocalizedString(@"Failed starting screen saver!", @"");
-		else
-			*errorString = NSLocalizedString(@"Failed stopping screen saver!", @"");
-		return NO;
+- (BOOL) execute: (NSString **) errorString {
+	// Unfortunately, ScreenSaverEngine doesn't support Scripting Bridge
+	// Use Cocoa API's to launch and terminate the app instead
+	
+	NSString *path = @"/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app";
+	NSString *identifier = [[NSBundle bundleWithPath: path] bundleIdentifier];
+	
+	if (turnOn) {
+		// launch screensaver
+		if ([[NSWorkspace sharedWorkspace] openFile: path])
+			return YES;
+	} else {
+		// terminate
+		NSArray *apps = [NSRunningApplication runningApplicationsWithBundleIdentifier: identifier];
+		[apps makeObjectsPerformSelector: @selector(terminate)];
+		return YES;
 	}
-
-	return YES;
+	
+	if (turnOn)
+		*errorString = NSLocalizedString(@"Failed starting screen saver!", @"");
+	else
+		*errorString = NSLocalizedString(@"Failed stopping screen saver!", @"");
+	return NO;
 }
 
 + (NSString *)helpText
