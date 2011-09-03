09dce6b309f0ce0d02a70eae346fb221f2d84f6b
David Jennes <david.jennes@gmail.com>
2011-09-03 07:07:49 +0200

    Tweak mail stmp action a bit (use name instead of hostname)

--- Source/MailSMTPServerAction.m
+++ Source/MailSMTPServerAction.m
@@ -59,15 +59,12 @@
 		MailApplication *Mail = [SBApplication applicationWithBundleIdentifier: @"com.apple.mail"];
 		
 		// find SMTP server
-		for (MailSmtpServer *server in [Mail smtpServers])
-			if ([server.serverName isEqualToString: hostname]) {
-				// apply to enabled accounts
-				for (MailAccount *account in [Mail accounts])
-					if (account.enabled)
-						account.deliveryAccount = server;
-				
-				break;
-			}
+		MailSmtpServer *server = [Mail.smtpServers objectWithName: hostname];
+		
+		// apply to enabled accounts
+		for (MailAccount *account in Mail.accounts)
+			if (account.enabled)
+				account.deliveryAccount = server;
 		
 	} @catch (NSException *e) {
 		*errorString = NSLocalizedString(@"Couldn't set IMAP server!", @"In MailIMAPServerAction");
@@ -79,13 +76,13 @@
 
 + (NSString *)helpText
 {
-	return NSLocalizedString(@"The parameter for MailSMTPServer actions is the hostname of the "
-				 "SMTP server to make the default for all Mail accounts.", @"");
+	return NSLocalizedString(@"The parameter for MailSMTPServer actions is the name of the "
+							 "SMTP server to make the default for all Mail accounts.", @"");
 }
 
 + (NSString *)creationHelpText
 {
-	return NSLocalizedString(@"Set Mail's SMTP server hostname to", @"");
+	return NSLocalizedString(@"Set Mail's SMTP server to", @"");
 }
 
 + (NSArray *) limitedOptions {
@@ -93,13 +90,13 @@
 	
 	@try {
 		MailApplication *Mail = [SBApplication applicationWithBundleIdentifier: @"com.apple.mail"];
-		SBElementArray *list = [Mail smtpServers];
+		SBElementArray *list = Mail.smtpServers;
 		opts = [NSMutableArray arrayWithCapacity:[list count]];
 		
 		// for each SMTP server
 		for (MailSmtpServer *server in list)
 			[opts addObject:[NSDictionary dictionaryWithObjectsAndKeys:
-							 server.serverName, @"option", server.serverName, @"description", nil]];
+							 server.name, @"option", server.name, @"description", nil]];
 		
 	} @catch (NSException *e) {
 		opts = [NSArray array];
