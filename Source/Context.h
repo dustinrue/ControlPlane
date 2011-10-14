098a701a311de29319d6e1ddb573e5dba220a9d6
David Jennes <david.jennes@gmail.com>
2011-10-14 16:43:07 +0200

    Flesh out the context class a bit

--- Source/Context.h
+++ Source/Context.h
@@ -6,16 +6,30 @@
 //  Copyright 2011. All rights reserved.
 //
 
+@class Action;
+@class Rule;
+
 @interface Context : NSObject {
 	BOOL m_active;
+	NSUInteger m_confidence;
 	NSString *m_name;
 	NSString *m_uuid;
 	NSMutableArray *m_actions;
 	NSMutableArray *m_rules;
+	
+	NSLock *m_actionsLock;
 }
 
-@property (readwrite, assign, nonatomic) BOOL active;
+- (void) addAction: (Action *) action;
+- (void) addRule: (Rule *) rule;
+- (void) removeAction: (Action *) action;
+- (void) removeRule: (Rule *) rule;
+
+@property (readwrite, assign) BOOL active;
+@property (readwrite, assign) NSUInteger confidence;
 @property (readwrite, copy) NSString *name;
 @property (readwrite, copy) NSString *uuid;
+@property (readonly) NSArray *actions;
+@property (readonly) NSArray *rules;
 
 @end
--- Source/Context.m
+++ Source/Context.m
@@ -7,11 +7,13 @@
 //
 
 #import "Context.h"
+#import "KVOAdditions.h"
 #import <Plugins/Actions.h>
 #import <Plugins/Rules.h>
 
 @interface Context (Private)
 
+- (void) ruleMatchChangedWithOld: (BOOL) oldMatch andNew: (BOOL) newMatch;
 - (void) activated;
 - (void) deactivated;
 + (NSString *) stringWithUUID;
@@ -20,23 +22,86 @@
 
 @implementation Context
 
+@synthesize actions = m_actions;
 @synthesize active = m_active;
+@synthesize confidence = m_confidence;
 @synthesize name = m_name;
+@synthesize rules = m_rules;
 @synthesize uuid = m_uuid;
 
 - (id) init {
 	self = [super init];
 	ZAssert(self, @"Unable to init super '%@'", NSStringFromClass(super.class));
 	
-	self.uuid = [Context stringWithUUID];
-	self.name = nil;
-	self.active = NO;
-	m_rules = [NSMutableArray new];
 	m_actions = [NSMutableArray new];
+	m_actionsLock = [NSLock new];
+	m_rules = [NSMutableArray new];
+	self.active = NO;
+	self.confidence = 0;
+	self.name = nil;
+	self.uuid = [Context stringWithUUID];
 	
 	return self;
 }
 
+#pragma mark - Actions
+
+- (void) addAction: (Action *) action {
+	@synchronized(m_actionsLock) {
+		ZAssert([self.actions containsObject: action], @"Context already owns action");
+		[m_actions addObject: action];
+	}
+}
+
+- (void) removeAction: (Action *) action {
+	@synchronized(m_actionsLock) {
+		ZAssert(![self.actions containsObject: action], @"Context doesn't own action");
+		[m_actions removeObject: action];
+	}
+}
+
+#pragma mark - Rules
+
+- (void) addRule: (Rule *) rule {
+	ZAssert([self.rules containsObject: rule], @"Context already owns rule");
+	[m_rules addObject: rule];
+	
+	// observe if needed
+	if (self.active)
+		[rule addObserver: self
+			   forKeyPath: @"match"
+				  options: NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld
+				 selector: @selector(ruleMatchChangedWithOld:andNew:)];
+}
+
+- (void) removeRule: (Rule *) rule {
+	ZAssert(![self.rules containsObject: rule], @"Context doesn't own rule");
+	[m_rules removeObject: rule];
+	
+	// stop observing
+	[rule removeObserver: self forKeyPath: @"match"];
+}
+
+- (void) ruleMatchChangedWithOld: (BOOL) oldMatch andNew: (BOOL) newMatch {
+	if (oldMatch == newMatch)
+		return;
+	
+	// recalculate our confidence level
+	NSUInteger confidence = 0;
+	for (Rule *rule in self.rules)
+		if (rule.match)
+			confidence += rule.confidence;
+	
+	// store it
+	if (self.confidence != confidence)
+		self.confidence = confidence;
+}
+
+#pragma mark - Activation
+
+- (BOOL) active {
+	return m_active;
+}
 
 - (void) setActive: (BOOL) active {
 	if (!m_active && active)
@@ -48,14 +113,20 @@
 }
 
 - (void) activated {
-	DLog(@"Activated context '%@', executing actions", self.name);
-	[ActionsManager.sharedActionsManager executeActions: m_actions when: kWhenEntering];
+	@synchronized(m_actionsLock) {
+		DLog(@"Activated context '%@', executing actions", self.name);
+		[ActionsManager.sharedActionsManager executeActions: m_actions when: kWhenEntering];
+	}
 }
 
 - (void) deactivated {
-	DLog(@"Deactivated context '%@', executing actions", self.name);
-	[ActionsManager.sharedActionsManager executeActions: m_actions when: kWhenLeaving];
+	@synchronized(m_actionsLock) {
+		DLog(@"Deactivated context '%@', executing actions", self.name);
+		[ActionsManager.sharedActionsManager executeActions: m_actions when: kWhenLeaving];
+	}
 }
+
+#pragma mark - Helper functions
 
 + (NSString *) stringWithUUID {
 	CFUUIDRef uuidObj = CFUUIDCreate(nil);
