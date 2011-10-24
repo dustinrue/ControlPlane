7dbcac19a540c7286382de9e9c34325c6c53e2cf
Dustin Rue <ruedu@dustinrue.com>
2011-10-23 20:17:25 -0500

    add comment, fix small typo

--- Utilities/remove_helper_tool.sh
+++ Utilities/remove_helper_tool.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# a simple utility for removing the ControlPlane helper app
+# *must* be run as root
 
 launchctl unload -F /Library/LaunchDaemons/com.dustinrue.ControlPlane.plist
 rm /Library/LaunchDaemons/com.dustinrue.ControlPlane.plist
-rm /Library/PrivilegedHelperTools//com.dustinrue.ControlPlane
+rm /Library/PrivilegedHelperTools/com.dustinrue.ControlPlane
