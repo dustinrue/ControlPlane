c91f83ed75915af9645fe98bde05f9785a17c5ac
Dustin Rue <ruedu@dustinrue.com>
2011-11-20 22:36:11 -0600

    still hangs when running the script

--- Source/ShellScriptEvidenceSource.m
+++ Source/ShellScriptEvidenceSource.m
@@ -63,16 +63,46 @@
 }
 
 - (BOOL)doesRuleMatch:(NSDictionary *)rule {
-    return false;
+    
+	NSMutableArray *args = [[NSMutableArray alloc] initWithCapacity:1];
+    
+	[args insertObject:[rule objectForKey:@"parameter"] atIndex:0];
+    
+    DSLog(@"attempting to run %@", [args objectAtIndex:0]);
+    
+	NSTask *task = [NSTask launchedTaskWithLaunchPath:@"/bin/sh" arguments:args];
+    
+	[task waitUntilExit];
+    DSLog(@"task ended");
 
+	if ([task terminationStatus] != 0) {
+	    [args release];
+
+		return NO;
+	}
+    	[args release];
+	return YES;
 }
 
 - (NSMutableDictionary *)readFromPanel {
-    
+    NSMutableDictionary *dict = [super readFromPanel];
+	
+	// store values
+	[dict setValue: currentFileName forKey: @"parameter"];
+    [dict setValue: currentFileName forKey: @"description"];
+    [dict setValue: @"ShellScript" forKey: @"type"];
+	
+	return dict;
 }
 
 - (void)writeToPanel:(NSDictionary *)dict usingType:(NSString *)type {
-    
+	[super writeToPanel: dict usingType: type];
+	
+
+	// show values
+    [self setCurrentFileName:[dict objectForKey:@"parameter"]];
+
+   
 }
 
 
