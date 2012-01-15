83d7bc8b0e0b2beecf19fe7738b3a66d6dfac520
Dustin Rue <ruedu@dustinrue.com>
2012-01-15 11:16:42 -0600

    cleanup copyright

--- Source/NSString+ShellScriptHelper.h
+++ Source/NSString+ShellScriptHelper.h
@@ -3,7 +3,7 @@
 //  ControlPlane
 //
 //  Created by Dustin Rue on 1/15/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
