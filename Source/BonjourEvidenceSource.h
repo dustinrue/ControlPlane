da7425718594963a1e6e5820b765482344af526f
VladimirTechMan <redhead.ru@gmail.com>
2013-08-23 18:13:26 +0400

    Major rework of Bonjour ES. Light non-blocking synchronization with GCD. Preventing any existing potential for dead locking (which does seem to happen in the field on some rare cases). Code is shorter and easier to read and maintain (I hope). Any feedback is very welcome, as usual.

--- Source/BonjourEvidenceSource.h
+++ Source/BonjourEvidenceSource.h
@@ -8,48 +8,13 @@
 #import "GenericEvidenceSource.h"
 #import "CPBonjourResolver.h"
 
-
-@interface BonjourEvidenceSource : GenericEvidenceSource<CPBonjourResolverDelegate> 
-
-@property (strong) NSLock *lock;
-
-// TODO: do we need an NSLock to protect this stuff?
-@property (assign) int stage;	// 0 = idle, 1 = searching for services, 2 = enumerating those services
-
-// this is the top level browser responsible for storing the
-// all found services in the local domain
-@property (strong) CPBonjourResolver *topLevelNetworkBrowser;
-
-// For each service on a host in cpBonjourResolvers
-//
-@property (strong) NSArray *servicesBeingResolved;
-
-// for each type of service that the top level browser
-// finds we create a new NSNetService object responsible
-// for figuring out what hosts actually offer that service
-@property (strong) NSMutableArray *cpBonjourResolvers;
-
-// full resolved services, we now know what service
-// and what host
-@property (strong) NSArray *services;
-
-// service level browsers by their type
-@property (strong) NSMutableDictionary *servicesByType;
-
-
-@property (strong) NSTimer *scanTimer;
-
-@property (strong) NSMutableArray *hits;
-@property (strong) NSMutableArray *hitsInProgress;
-
+@interface BonjourEvidenceSource : GenericEvidenceSource<CPBonjourResolverDelegate,NSNetServiceDelegate>
 
 - (id)init;
 - (void)dealloc;
 
 - (void)start;
 - (void)stop;
-
-- (void)clearCollectedData;
 
 - (NSString *)name;
 - (BOOL)doesRuleMatch:(NSDictionary *)rule;
--- Source/CPBonjourResolver.m
+++ Source/CPBonjourResolver.m
@@ -5,67 +5,84 @@
 //  Created by Dustin Rue on 3/2/12.
 //  Copyright (c) 2012 Dustin Rue. All rights reserved.
 //
+//  Rework and cleanup done by Vladimir Beloborodov (VladimirTechMan) on 22-23 Aug 2013.
+//
+//  IMPORTANT: This code is intended to be compiled for the ARC mode
+//
 
 #import "CPBonjourResolver.h"
 #import "DSLogger.h"
 
-@implementation CPBonjourResolver 
+@interface CPBonjourResolver ()
 
-@synthesize delegate;
-@synthesize foundItems;
-@synthesize myServiceType;
+@property (strong) NSMutableArray *foundItems;
+
+@end
+
+@implementation CPBonjourResolver {
+    NSNetServiceBrowser *networkBrowser;
+}
 
 #pragma mark -
 #pragma mark init/dealloc
 - (id) init {
     self = [super init];
-    
-    if (!self)
-        return self;
+    if (!self) {
+        return nil;
+    }
 
     networkBrowser = [[NSNetServiceBrowser alloc] init];
-    foundItems = [[NSMutableArray alloc] init];
-    
+    [networkBrowser scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+
+    _foundItems = [[NSMutableArray alloc] init];
+
     return self;
 }
 
-- (void) dealloc {
-    [networkBrowser release];
-    [foundItems release];
-    [super dealloc];
+- (void)dealloc {
 }
 
-
-- (void) searchForServicesOfType:(NSString *)serviceType inDomain:(NSString *) searchDomain {
-    [networkBrowser setDelegate:self];
+- (void)searchForServicesOfType:(NSString *)serviceType inDomain:(NSString *)searchDomain {
+    networkBrowser.delegate = self;
     [networkBrowser searchForServicesOfType:serviceType inDomain:searchDomain];
 }
 
-- (void) stop {
+- (void)stop {
     [networkBrowser stop];
-    [foundItems removeAllObjects];
-}
-
-- (void) doResolveForService:(NSNetService *)service {
-
-    [service setDelegate:self];
-    [service resolveWithTimeout:2];
-
+    [self.foundItems removeAllObjects];
 }
 
 #pragma mark -
-#pragma mark NSNetServiceBrowser and NSNetService delegates
+#pragma mark NSNetServiceBrowser delegates
 - (void)netServiceBrowser:(NSNetServiceBrowser *)netServiceBrowser
          didRemoveService:(NSNetService *)netService
                moreComing:(BOOL)moreServicesComing {
+    if (![self.delegate respondsToSelector:@selector(netServiceBrowser:removedService:)]) {
+        DSLog(@"Delegate is not compliant with CPBonjourResolverDelegate"
+              " so you missed hearing about a service that went away");
+        return;
+    }
 
-    [foundItems removeObject:netService];
-    
-    if ([[self delegate] respondsToSelector:@selector(netServiceBrowser:removedService:)])
-        [[self delegate] netServiceBrowser:self removedService:netService];
-    else
-        DSLog(@"you're not compliant with CPBonjourResolverDelegate so you missed hearing about this service that went away");
-    
+    [self.delegate netServiceBrowser:self removedService:netService];
+    [self.foundItems removeObject:netService];
+}
+
+- (void)netServiceBrowser:(NSNetServiceBrowser *)netServiceBrowser
+           didFindService:(NSNetService *)netService
+               moreComing:(BOOL)moreServicesComing {
+    if (![self.delegate respondsToSelector:@selector(foundNewServiceFrom:withService:)]) {
+        DSLog(@"Delegate is not compliant with CPBonjourResolverDelegate"
+              " so you missed hearing about a service that is available");
+        return;
+    }
+
+    [self.foundItems addObject:netService];
+    [self.delegate foundNewServiceFrom:self withService:netService];
+}
+
+- (void)netServiceBrowser:(NSNetServiceBrowser *)netServiceBrowser
+             didNotSearch:(NSDictionary *)errorInfo {
+	DSLog(@"Failed to perform search:\n%@", errorInfo);
 }
 
 - (void)netServiceBrowserDidStopSearch:(NSNetServiceBrowser *)netServiceBrowser {
@@ -74,63 +91,29 @@
 #endif
 }
 
-- (void)netServiceBrowser:(NSNetServiceBrowser *)netServiceBrowser
-             didNotSearch:(NSDictionary *)errorInfo {
-	DSLog(@"Failed to perform search:\n%@", errorInfo);
-}
-
-- (void)netServiceBrowser:(NSNetServiceBrowser *)netServiceBrowser
-           didFindService:(NSNetService *)netService
-               moreComing:(BOOL)moreServicesComing {
-    
-    [foundItems addObject:netService];
-    
-    if ([[self delegate] respondsToSelector:@selector(foundNewServiceFrom:withService:)]) {
-        [[self delegate] foundNewServiceFrom:self withService:netService];
-    }
-}
-
-
-- (void)netServiceDidResolveAddress:(NSNetService *)sender {
-    if ([[self delegate] respondsToSelector:@selector(resolvedServiceArrived:)])
-        [[self delegate] resolvedServiceArrived:sender];
-    else
-        DSLog(@"you're not compliant with CPBonjourResolverDelegate so you missed on this hot new service I found");
-}
-
-
-- (void)netService:(NSNetService *)sender didNotResolve:(NSDictionary *)errorDict {
-        DSLog(@"failed to resolve netService: %@", errorDict);
-}
-
-
-- (void)netServiceBrowser:(NSNetServiceBrowser *)aNetServiceBrowser didFindDomain:(NSString *)domainString moreComing:(BOOL)moreComing {
-#ifdef DEBUG_MODE
-    DSLog(@"");
-#endif
-}
-    
-
-
-
-- (void)netServiceBrowser:(NSNetServiceBrowser *)aNetServiceBrowser didRemoveDomain:(NSString *)domainString moreComing:(BOOL)moreComin {
+- (void)netServiceBrowser:(NSNetServiceBrowser *)aNetServiceBrowser
+            didFindDomain:(NSString *)domainString
+               moreComing:(BOOL)moreComing {
 #ifdef DEBUG_MODE
     DSLog(@"");
 #endif
 }
 
-- (NSUInteger) numberOfHosts {
-    return [foundItems count];
+- (void)netServiceBrowser:(NSNetServiceBrowser *)aNetServiceBrowser
+          didRemoveDomain:(NSString *)domainString
+               moreComing:(BOOL)moreComin {
+#ifdef DEBUG_MODE
+    DSLog(@"");
+#endif
 }
-
 
 #pragma mark -
 #pragma mark Utility methods
-+ (NSString *) stripLocal:(NSString *) incoming {
-    if ([incoming hasSuffix:@".local."])
-        return[incoming substringToIndex:([incoming length] - 6)];
-    
++ (NSString *)stripLocal:(NSString *)incoming {
+    if ([incoming hasSuffix:@".local."]) {
+        return [incoming substringToIndex:([incoming length] - 6)];
+    }
     return incoming;
-    
 }
+
 @end
