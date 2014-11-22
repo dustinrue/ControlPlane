3aad26155d79a927a4e3aaab951bb44fece6d372
Eric Betts <bettse@gmail.com>
2014-11-21 20:23:12 -0800

    Empty files for NFCMirrorEvidenceSource

---
+++ Source/NFCMirrorEvidenceSource.h
@@ -0,0 +1,13 @@
+//
+//  NFCMirrorEvidenceSource.h
+//  ControlPlane
+//
+//  Created by Eric Betts on 11/21/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NFCMirrorEvidenceSource : NSObject
+
+@end
---
+++ Source/NFCMirrorEvidenceSource.m
@@ -0,0 +1,13 @@
+//
+//  NFCMirrorEvidenceSource.m
+//  ControlPlane
+//
+//  Created by Eric Betts on 11/21/14.
+//
+//
+
+#import "NFCMirrorEvidenceSource.h"
+
+@implementation NFCMirrorEvidenceSource
+
+@end
